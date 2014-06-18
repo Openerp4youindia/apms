d018370b55ffb35e564cc02caa2bc79b13f01496
Sujeet <sujeet@Sujeet-PC.(none)>
2014-06-18 17:35:04 +0530

    Added account base module

---
+++ account_base/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011-2015 OpenERP4you (<http://openerp4you.in>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    "name" : "Accounting Base Properties",
+    "version" :"1.0",
+    "depends" : [
+        "base","account"
+    ],
+    "author" : 'OpenERP4you',
+    "description": """Accounting Base Properties is providing the,
+    Basic properties for the Customer / Suppliers for the Accounting
+    i.e. PAN No, TIN No, Sales Tax No, and Exise related Information
+    """,
+    "website" : 'http://openerp4you.in',
+    "category" : "Generic Modules/Indian Accounting",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+        "partner_view.xml",
+        "wizard/account_setup_view.xml"
+    ],
+    "active": False,
+    "installable": True
+}
